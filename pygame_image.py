779dec0d79f7f57685a0079225689ab4b10c7b74
c0b23137 <c0b2313715@edu.teu.ac.jp>
2024-09-24 16:06:40 +0900

    　ワープ解消

--- pygame_image.py
+++ pygame_image.py
@@ -10,15 +10,19 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    gb_img =pg.transform.flip(bg_img,True,False)
     kt_img=pg.image.load("fig/3.png")
     kt_img=pg.transform.flip(kt_img,True,False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        time=tmr%800
+        time=tmr%3200
             
-        screen.blit(bg_img, [0-time, 0])
+        screen.blit(bg_img, [-time, 0])
+        screen.blit(gb_img,[1600-time,0])
+        screen.blit(bg_img, [3200-time, 0])
+        screen.blit(gb_img,[4800-time,0])
         screen.blit(kt_img,[300,200])
         pg.display.update()
         tmr += 1        
