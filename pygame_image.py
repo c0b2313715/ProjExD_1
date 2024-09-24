ae067943cc1a51119c5ca8f2273a9595005c5aa7
c0b23137 <c0b2313715@edu.teu.ac.jp>
2024-09-24 15:46:37 +0900

    　間延び解消

--- pygame_image.py
+++ pygame_image.py
@@ -16,8 +16,9 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0-tmr, 0])
+        time=tmr%800
+            
+        screen.blit(bg_img, [0-time, 0])
         screen.blit(kt_img,[300,200])
         pg.display.update()
         tmr += 1        
