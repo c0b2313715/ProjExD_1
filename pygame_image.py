772487b30c6610967597a530fdd7af6a7c663256
c0b23137 <c0b2313715@edu.teu.ac.jp>
2024-09-24 16:26:06 +0900

    　こうかとん移動

--- pygame_image.py
+++ pygame_image.py
@@ -13,17 +13,29 @@ def main():
     gb_img =pg.transform.flip(bg_img,True,False)
     kt_img=pg.image.load("fig/3.png")
     kt_img=pg.transform.flip(kt_img,True,False)
+    kt_rct=kt_img.get_rect()
+    kt_rct.center=300,200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst= pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kt_rct.move_ip((0,-1))
+        elif key_lst[pg.K_DOWN]:
+            kt_rct.move_ip((0,+1))
+        elif key_lst[pg.K_LEFT]:
+            kt_rct.move_ip((-1,0))
+        elif key_lst[pg.K_RIGHT]:
+            kt_rct.move_ip((+1,0))
+
         time=tmr%3200
             
         screen.blit(bg_img, [-time, 0])
         screen.blit(gb_img,[1600-time,0])
         screen.blit(bg_img, [3200-time, 0])
         screen.blit(gb_img,[4800-time,0])
-        screen.blit(kt_img,[300,200])
+        screen.blit(kt_img,kt_rct)
         pg.display.update()
         tmr += 1        
         clock.tick(200)
